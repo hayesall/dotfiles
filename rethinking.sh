f81906bef1d8674c4e888e1f240c7acd4bcc391a
Alexander L. Hayes <alexander@batflyer.net>
2023-04-20 22:11:36 -0400

    🔨 Add miniconda notes to `rethinking.sh`

--- rethinking.sh
+++ rethinking.sh
@@ -15,3 +15,16 @@ sudo apt install zsh
 
 # Maybe embed this directly in the dotfiles?
 curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark --output ~/.dircolors
+
+# Setup miniconda
+
+curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --output ~/setup_miniconda.sh
+
+bash ~/setup_miniconda.sh -b
+
+/home/hayesall/miniconda3/bin/python /home/hayesall/miniconda3/bin/conda init
+
+# Turn off the feature where conda auto-activates `base`
+/home/hayesall/miniconda3/bin/conda init config --set auto_activate_base false
+
+rm -f ~/setup_miniconda.sh
