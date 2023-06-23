8a06350b841a759d14637ed35bcb278bcc98f0c4
Alexander L. Hayes <alexander@batflyer.net>
2023-06-23 11:03:32 -0400

    ✨ Separate download from install step, add `-y`
    This should run the Rust installer in batch mode, avoiding the prompt.

--- rust_installation.sh
+++ rust_installation.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup-init.sh
+
+sh rustup-init.sh -y
+
+rm -f rustup-init.sh
