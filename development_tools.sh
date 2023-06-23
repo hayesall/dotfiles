2fa958b7d4235dcfbccdf1c7faf7afff09dbb4df
Alexander L. Hayes <alexander@batflyer.net>
2023-06-23 11:36:21 -0400

    🐛 Drop ninja-build and python3-venv

--- development_tools.sh
+++ development_tools.sh
@@ -4,4 +4,4 @@ apt update
 
 apt upgrade -y
 
-apt install -y build-essential gcc g++ gfortran python3-dev python3-venv make cmake ninja-build pkg-config autoconf
+apt install -y build-essential gcc g++ gfortran python3-dev make cmake pkg-config autoconf
