d279cfebf0024285f048cfbf7b3cfcb06b945522
Alexander L. Hayes <alexander@batflyer.net>
2023-06-23 11:14:09 -0400

    ✨ Add `cargo_installs.sh` with my preferred packages

---
+++ cargo_installs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# https://github.com/sharkdp/bat
+cargo install --locked bat
+
+# https://github.com/ajeetdsouza/zoxide
+cargo install --locked zoxide
+
+# https://github.com/bootandy/dust
+cargo install du-dust
+
+# https://github.com/sharkdp/fd
+cargo install fd-find
+
+# https://github.com/ogham/exa
+cargo install exa
+
+# https://github.com/BurntSushi/ripgrep
+cargo install ripgrep
+
+# https://github.com/casey/just
+cargo install just
