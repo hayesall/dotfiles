224eb8a27853f463772bf5a21a9c43071f0e2198
hayesall <hayesall@iu.edu>
2023-07-07 11:06:56 -0400

    🐛 Fix missing sudo on go install

--- go_install.sh
+++ go_install.sh
@@ -4,7 +4,7 @@
 
 curl -L https://go.dev/dl/go1.20.5.linux-amd64.tar.gz --output go1.20.5.linux-amd64.tar.gz
 
-sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.5.linux-amd64.tar.gz
 
 # Add: export PATH=$PATH:/usr/local/go/bin
 
